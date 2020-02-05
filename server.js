fbdef10de12ea098d08dd69df7366a8f1b097ff6
Ankit Sharma <ankit_sharma@comcast.com>
2020-02-05 16:07:43 +0530

    adding DB config and connection models

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 'use strict';
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-// Imports dependencies and set up http server
 const 
+  colors = require('colors'),
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express(); // creates express http server
+
+
+
+const connectMongo = require('./lib/dbModels/connection');
+
+//for now I am keeping it as an asynchronous connection
+//later on might need to start the app once this DB connection is set.
+connectMongo();
 
 app.disable('x-powered-by');
 app.use(bodyParser.json());
@@ -15,4 +22,4 @@ const routes = require('./lib/router');
 app.use('/', routes);
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 9447, () => console.log('Whis'.green + ' is up and ready to serve on port: '.blue + '9447'.green));
